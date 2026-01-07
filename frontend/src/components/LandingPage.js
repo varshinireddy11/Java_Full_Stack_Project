652aa1fef8654541218b4f0e981d53abacfad11e
varshini reddy <sunithavaru1234@gmail.com>
2026-01-07 11:46:29 +0530

    updating header file

--- frontend/src/components/LandingPage.js
+++ frontend/src/components/LandingPage.js
@@ -44,7 +44,7 @@ const LandingPage = () => {
               fontSize: isSmallScreen ? '2rem' : 'clamp(2rem, 5vw, 3rem)',
             }}
           >
-            Welcome to Employee Management System
+            Welcome 
           </Typography>
           <Typography
             variant="h6"
